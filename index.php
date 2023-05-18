6a63d1ce7a7d9abce5801efb82c8d70ac62f86b4
unknown <email>
2023-05-18 19:33:31 +0700

    tambahan j-query

--- index.php
+++ index.php
@@ -2,21 +2,34 @@
 <html>
 <head>
     <title>Beranda</title>
+    <script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.css" />
+    <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+
+    
+    
 </head>
 <body>
 
-<div class="container">
-<table cellpadding="10" cellspacing="0" class="table table-striped">
+<div class="container" ">
+<table  id="mytable" class="display" cellpadding="10" cellspacing="0" class="table table-striped">
   <a href="create.php" class="btn btn-primary mt-4 mb-4">Tambah data</a>
-        <tr>
-            <td widht="50px" class="text-center">ID</td>
-            <td widht="200px" class="text-center">Nama Pengarang</td>
-            <td widht="200px" class="text-center">Email</td>
-            <td widht="300px" class="text-center">Telpon</td>
-            <td widht="200px" class="text-center">Alamat</td>
-            <td widht="200px" class="text-center">Aksi</td>
-        </tr>
+        <thead>
+            <tr>
+                <th widht="50px" class="text-center">ID</th>
+                <th widht="200px" class="text-center">Nama Pengarang</th>
+                <th widht="200px" class="text-center">Email</th>
+                <th widht="300px" class="text-center">Telpon</th>
+                <th widht="200px" class="text-center">Alamat</th>
+                <th widht="200px" class="text-center">Aksi</th>
+                
+            </tr>  
+        </thead>
+  
+       
+            
+        
         <?php
             include ('connect.php');
             $data_pengarang = mysqli_query($conn, "SELECT * FROM pengarang");
@@ -39,3 +52,9 @@
     </div>
     </body>
 </html>
+<script type="text/javascript">
+    $(document).ready(function(){
+        $('#mytable').DataTable();
+
+    });
+</script>
